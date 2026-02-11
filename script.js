db6e67d37e381abca08ef3f2f600206c22a463ba
SashaMakin <abutomin@gmail.com>
2026-02-11 16:30:11 +0300

    11022026

--- script.js
+++ script.js
@@ -42,3 +42,27 @@ repairclosePopupButton.addEventListener('click', function () {
   repairclosePopupButton.classList.add('show');
   repairopenPopupButton.classList.remove('show');
 }});
+
+var headerOpenButton = document.querySelector('.header_button_open');
+var showMenu = document.querySelector('.mobilaside');
+var closwMenu = document.querySelector('.closure');
+var showOverlay = document.querySelector('.overlay');
+
+headerOpenButton.addEventListener('click', function () {
+  showMenu.classList.remove('menuMobile');
+  showOverlay.classList.remove('overlayShow');
+});
+
+closwMenu.addEventListener('click', function (event) {
+  showMenu.classList.add('menuMobile');
+  showOverlay.classList.add('overlayShow');
+});
+
+showOverlay.addEventListener('click', function (event) {
+  showMenu.classList.add('menuMobile');
+  showOverlay.classList.add('overlayShow');
+  showCall.classList.add('menuCallMobileShow');
+  showCallPk.classList.add('menuCallShow');
+  showMail.classList.add('menuMailMobileShow');
+  showMailPk.classList.add('menuMailShow');
+});
